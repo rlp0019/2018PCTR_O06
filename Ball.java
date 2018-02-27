c0472cca8934367282805d92eb5efd1517511554
rlp0019 <rlp0019@alu.ubu.es>
2018-02-27 18:03:32 +0100

    Revert "Revert "TODO imagen bola""
    This reverts commit 32db91ce5e59bceec0e5dfed96ba80c83f2abcf0.

--- Ball.java
+++ Ball.java
@@ -4,7 +4,6 @@ import javax.swing.ImageIcon;
 
 //TODO Transform the code to be used safely in a concurrent context.  
 public class Ball {
-	// TODO Find an archive named Ball.png
 	private String Ball = "Ball.png";
 
 	private double x, y, dx, dy;
